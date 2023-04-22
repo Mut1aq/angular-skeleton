d6131b3ae38ac463e0a500f1deac53d4adb84701
Mutlaq The Great <mutlaqalsadeed@gmail.com>
2023-04-22 21:45:23 +0300

    solve duplication in base component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BaseComponent } from './views/base/base.component';
 
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () =>
+      import('./views/pages/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
     path: '',
-    component: BaseComponent,
     children: [
-      {
-        path: 'auth',
-        loadChildren: () =>
-          import('./views/pages/auth/auth.module').then((m) => m.AuthModule),
-      },
       {
         path: 'mail',
         loadChildren: () =>
